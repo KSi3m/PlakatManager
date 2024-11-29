fe5ea78f7e704ab327325b26333c72091a6062aa
ksi3m <mieszko9919@gmail.com>
2024-11-29 22:01:39 +0100

    User controller and myelectionitems endpoint added

---
+++ PlakatManager/PlakatManager/CQRS/Queries/UserQueries/GetUsersElectionItems/GetUsersElectionItemsQuery.cs
@@ -0,0 +1,11 @@
+﻿using ElectionMaterialManager.CQRS.Responses;
+using ElectionMaterialManager.Dtos;
+using MediatR;
+
+namespace ElectionMaterialManager.CQRS.Queries.UserQueries.GetUsersElectionItems
+{
+    public class GetUsersElectionItemsQuery: IRequest<GenericResponseWithList<ElectionItemDto>>
+    {
+
+    }
+}
---
+++ PlakatManager/PlakatManager/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using ElectionMaterialManager.CQRS.Queries.ElectionItemQueries.GetElectionItems;
+using ElectionMaterialManager.CQRS.Queries.UserQueries.GetUsersElectionItems;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectionMaterialManager.Controllers
+{
+
+    [Route("api/v1/")]
+    [ApiController]
+    public class UserController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("user/election-items")]
+        public async Task<IActionResult> GetMyElectionItems()
+        {
+
+            var query = new GetUsersElectionItemsQuery();
+            var response = await _mediator.Send(query);
+            if (response.Success)
+                return Ok(response);
+            return BadRequest(new { response.Message });
+        }
+    }
+}
